b7d900b41934487123218cc098786aceead95863
Barnold88 <bandslover@outlook.com>
2025-06-18 18:03:16 +0100

    Finish getPrices function

--- scripts/content.js
+++ scripts/content.js
@@ -1,19 +1,30 @@
-
 function getPrices(){
 
   priceElements = document.getElementsByClassName("price-section")
+  priceSymbol = "?"
+
+  runningTotal = 0.0
+
+  if(priceElements.length >=1){
+    symbols = priceElements[0].getElementsByClassName("a-price-symbol")
+    if(symbols.length >=1 ){
+      priceSymbol = symbols[0].innerText
+    }
+  } 
 
   for (let item of priceElements) {
 
     whole = item.getElementsByClassName("a-price-whole")
     fraction = item.getElementsByClassName("a-price-fraction")
     if(whole.length >=1 && fraction.length >=1){
-      console.log(item,`${whole[0].innerText}${fraction[0].innerText}`)
+      price = `${whole[0].innerText}${fraction[0].innerText}`
+      price = parseFloat(price);
+      runningTotal += price
     }
-
-    
   }
+  return `${priceSymbol}${runningTotal}`
+  
 }
 
 
-getPrices()
+
